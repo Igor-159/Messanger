a2cfeffa7879fd8bfb60393890dd7ecca0abc8c2
Igor-159 <igor.mokrushin.159@mail.ru>
2022-02-28 19:49:33 +0500

    build

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import ProfileContainer from './components/profile/ProfileContainer';
 import HeaderContainer from './components/header/HeaderContainer';
 import NavBar from './components/navbar/NavBar';
 import UsersContainer from './components/user/UsersContainer';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {connect} from 'react-redux';
-import DialogsContainer from './components/dialogs/DialogsConteiner';
+import {connect} from 'react-redux';//import DialogsContainer from './components/dialogs/DialogsConteiner';
 import './App.css';
 import Login from './components/login/Login';
-import React from 'react';
+import React,{Suspense} from 'react';
 import { initializeApp} from './redux/app-reducer';
 import Spinner from './components/spinner/Spinner';
 
-
-
+const DialogsContainer = React.lazy(() => import('./components/dialogs/DialogsConteiner'));
+const ProfileContainer = React.lazy(() => import('./components/profile/ProfileContainer'));
 
 class App extends React.Component {
     componentDidMount(){
@@ -32,13 +30,14 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <NavBar/>
                 <div className='app-wraper-content'>
+                <Suspense fallback={<Spinner/>}>
                     <Routes>
                         <Route path='/dialogs' element={<DialogsContainer />}/>
                         <Route path='/profile/:id' element={<ProfileContainer/>}/>
                         <Route path='/users' element={<UsersContainer/>}/>
                         <Route path='/login' element={<Login/>}/>
                     </Routes>
-
+                </Suspense>
                 </div>
 
             </div>
